7eb453dc24515522f35fc741e7110d3aa736d4ed
RiyadRamadan <yeteplomtr@gmail.com>
2025-07-06 00:28:14 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,39 @@
+
+const CACHE_VERSION = 'pwa-cache-v2';
+
 self.addEventListener('install', (event) => {
     console.log('📦 Service Worker Installed');
     event.waitUntil(
-        caches.open('pwa-cache').then((cache) => {
+        caches.open(CACHE_VERSION).then((cache) => {
             return cache.addAll([
                 './index.html',
                 './main.js',
-                './manifest.json',
-                // Add other assets like CSS or images if any
+                './manifest.json'
             ]);
         })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keyList) =>
+            Promise.all(
+                keyList
+                    .filter((key) => key !== CACHE_VERSION)
+                    .map((key) => caches.delete(key))
+            )
+        )
     );
 });
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
+            return response || fetch(event.request).catch(() => {
+                if (event.request.destination === 'document') {
+                    return caches.match('./index.html');
+                }
+            });
         })
     );
 });
