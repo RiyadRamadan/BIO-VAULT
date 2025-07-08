f9842ecdb07c491f0d507636512776a4aa2f39a5
RiyadRamadan <yeteplomtr@gmail.com>
2025-07-08 18:45:14 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,39 +1,47 @@
 const CACHE_VERSION = 'pwa-cache-v3';
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_VERSION).then(cache =>
-      cache.addAll([
-        './index.html',
-        './main.js',
-        './manifest.json',
-        './icon-192.png',
-        './icon-512.png'
-      ])
-    )
+    caches.open(CACHE_VERSION).then((cache) => {
+      return cache.addAll([
+        './index.html', './main.js', './manifest.json'
+      ]);
+    })
   );
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
+    caches.keys().then((keyList) =>
       Promise.all(
-        keys.filter(key => key !== CACHE_VERSION)
-            .map(key => caches.delete(key))
+        keyList.filter((key) => key !== CACHE_VERSION)
+          .map((key) => caches.delete(key))
       )
     )
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response =>
-      response ||
-      fetch(event.request).catch(() =>
-        event.request.destination === 'document'
-          ? caches.match('./index.html')
-          : undefined
-      )
-    )
-  );
+self.addEventListener('fetch', (event) => {
+  const url = event.request.url;
+  if (url.includes('/api/')) {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const respClone = response.clone();
+          caches.open(CACHE_VERSION).then(cache => cache.put(event.request, respClone));
+          return response;
+        })
+        .catch(() => caches.match(event.request))
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request).catch(() => {
+          if (event.request.destination === 'document') {
+            return caches.match('./index.html');
+          }
+        });
+      })
+    );
+  }
 });
